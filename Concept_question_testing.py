9fa534c5593bd57ddac11e3b44ab3d26ddda8b26
Susana Espinosa <susanaesbel@hotmail.com>
2023-07-30 17:52:39 +0100

    tester commit

--- Concept_question_testing.py
+++ Concept_question_testing.py
@@ -2,7 +2,7 @@ import unittest
 from Concept_questions import is_isogram
 
 class IsIsogramTestCase(unittest.TestCase):
-
+##Tester Comment
     def test_case_insensitive(self):
         # The function should be case-insensitive, so the result should be the same regardless of letter casing
         self.assertTrue(is_isogram('IsOgRaM'))
